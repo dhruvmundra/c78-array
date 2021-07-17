99c42a6b2c8de848d272f7b249f755f3bfdf7f9b
dhruvmundra <81858843+dhruvmundra@users.noreply.github.com>
2021-07-17 16:45:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var fruits=["avacado","star fruit","dragon fruit"]
+document.getElementById("declaration_of_arrays").innerHTML=fruits;
+
+var first_fruit=fruits[0];
+console.log(first_fruit);
+document.getElementById("access_first_element").innerHTML=first_fruit;
+
+var second_fruit=fruits[1];
+console.log(second_fruit);
+document.getElementById("access_second_element").innerHTML=second_fruit;
+
+var third_fruit=fruits[2];
+console.log(third_fruit);
+document.getElementById("access_third_element").innerHTML=third_fruit;
+
+var size_of_array=fruits.length;
+console.log(size_of_array_fruit);
+document.getElementById("display_length_of_array").innerHTML=size_of_array;
